eb9694ec95949404451072a3602754c04be45c73
Sonya <yasenkovasofia@gmail.com>
2025-08-23 17:42:39 +0100

    bug_fix_chat

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, SetTaskStatusView
 
-urlpatterns = [ path('', TaskListView.as_view(), name='tasks_list') ]
+urlpatterns = [
+    path('', TaskListView.as_view(), name='tasks_list'),
+    path('set-status/', SetTaskStatusView.as_view(), name='tasks_set_status'),
+]
